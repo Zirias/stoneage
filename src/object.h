c56721007d6341f2cddc08dea589b9671e73d242
zirias <zirias@1df5a459-922f-45a1-9398-30de3e8b64c2>
2010-05-13 14:33:51 +0000

    better object management
    git-svn-id: https://svn.home.palmen-it.de/repo/stoneage/trunk@3 1df5a459-922f-45a1-9398-30de3e8b64c2

--- src/object.h
+++ src/object.h
@@ -3,11 +3,6 @@
 
 #include "common.h"
 
-enum ClassType {
-    CEntity,
-    CObject
-};
-
 struct TypeList;
 typedef struct TypeList *TypeList;
 
@@ -19,20 +14,20 @@ typedef struct Object *Object;
 
 #define CLASS() struct Object _object
 #define INHERIT(baseclass) struct baseclass _object
-#define CAST(pointer, type) ((type)GetObjectOf(pointer, C##type))
+#define CAST(pointer, type) ((type)GetObjectOf(pointer, CLASS_##type))
 #define NEW(T) T##_ctor((T)XMALLOC(struct T, 1), 0)
 #define DELETE(T, object) T##_dtor(object); XFREE(object)
 
 #define INHERIT_INIT(pointer, myclass, baseclass) \
-    types = RegisterType(types, C##myclass); \
+    types = RegisterType(types, CLASS_##myclass); \
     baseclass##_ctor((baseclass)pointer, types)
 #define OBJECT_INIT(pointer, myclass) INHERIT_INIT(pointer, myclass, Object)
 
 extern Object Object_ctor(Object o, TypeList types);
 extern void Object_dtor(Object o);
-extern enum ClassType GetType(void const *object);
+extern Type GetType(void const *object);
 extern Object GetObject(void const *object);
-extern Object GetObjectOf(void const *object, const enum ClassType T);
-extern TypeList RegisterType(TypeList types, const enum ClassType T);
+extern Object GetObjectOf(void const *object, const Type T);
+extern TypeList RegisterType(TypeList types, const Type T);
 
 #endif
