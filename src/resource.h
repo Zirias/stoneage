12e57be21aecdf47e8800f58a3941969fb8c0efb
zirias <zirias@1df5a459-922f-45a1-9398-30de3e8b64c2>
2010-05-15 19:30:28 +0000

    more documentation
    git-svn-id: https://svn.home.palmen-it.de/repo/stoneage/trunk@32 1df5a459-922f-45a1-9398-30de3e8b64c2

--- src/resource.h
+++ src/resource.h
@@ -7,23 +7,77 @@
 
 struct Resource_impl;
 
+/** @file resource.h
+ * Includes definition of the Resource class
+ */
+
+/** Represents a single Resource.
+ * A Resource object holds a block of binary data, identified by a string
+ * key.
+ */
 CLASS(Resource)
 {
     INHERIT(Object);
 
-    struct Resource_impl *pimpl;
+    struct Resource_impl *pimpl; /**< @private */
 
+    /** Set the Resource's name.
+     * Set's the string key for the resource. The maximum lenght is 255
+     * characters, longer names will be silently cut off. The name data is
+     * actually copied.
+     * @param name the name for the Resource
+     */
     void FUNC(setName) ARG(const char *name);
+
+    /** Get the Resource's name.
+     * @return a pointer to the name of the Resource
+     */
     const char *FUNC(getName) ARG();
 
+    /** Give a block of data to the Resource
+     * Sets the Resource's data to the given block. No copying is done,
+     * the Resource object takes ownership of the data.
+     * @param data pointer to the new data
+     * @param dataSize size of the new data in bytes
+     * @return nonzero if the Resource already contained data
+     */
     int FUNC(giveData) ARG(void *data, size_t dataSize);
+
+    /** Set data by copying it.
+     * Sets the Resource's data by copying bytes from a given location.
+     * @param data the source for the copy
+     * @param dataSize the number of bytes to copy
+     * @return nonzero if the Resource already contained data
+     */
     int FUNC(copyDataFrom) ARG(const void *data, size_t dataSize);
+
+    /** Set data by reading from a file stream.
+     * Sets the Resource's data by reading bytes from an opened file stream
+     * @param file the file stream to read from
+     * @param dataSize the number of bytes to read
+     * @return nonzero on read error or if the Resource already contained data
+     */
     int FUNC(readDataFrom) ARG(FILE *file, size_t dataSize);
 
+    /** Get the size of the Resource data.
+     * @return the number of bytes of the Resource's data
+     */
     size_t FUNC(getDataSize) ARG();
+
+    /** Get the Resource's data.
+     * @return a pointer to the Resource's data
+     */
     const void *FUNC(getData) ARG();
+
+    /** Take ownership of the Resource's data.
+     * Returns a pointer to the Resource's data and makes the Resource object
+     * forget about it.
+     * @return a pointer to the data, must be released after use
+     */
     void *FUNC(takeData) ARG();
 
+    /** Delete the Resource's data
+     */
     void FUNC(deleteData) ARG();
 };
 
