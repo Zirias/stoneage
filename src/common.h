8a53be41a195c05afabb4f26a392753c6579c251
zirias <zirias@1df5a459-922f-45a1-9398-30de3e8b64c2>
2010-05-15 11:09:19 +0000

    create resource as part of the build process
    git-svn-id: https://svn.home.palmen-it.de/repo/stoneage/trunk@24 1df5a459-922f-45a1-9398-30de3e8b64c2

--- src/common.h
+++ src/common.h
@@ -5,6 +5,11 @@
 #include "config.h"
 #endif
 
+#define QUOTEME_(x) #x
+#define QUOTEME(x) QUOTEME_(x)
+
+#define RES_GFX QUOTEME(DATADIR) "/gfx.sar"
+
 #include "classes.h"
 #include "log.h"
 #include "mm.h"
