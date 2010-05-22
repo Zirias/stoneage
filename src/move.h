0c5b7662f39bdd46e26b3d83880d13593dfe9044
zirias <zirias@1df5a459-922f-45a1-9398-30de3e8b64c2>
2010-05-22 17:10:06 +0000

    implementing class Move...
    git-svn-id: https://svn.home.palmen-it.de/repo/stoneage/branches/z@81 1df5a459-922f-45a1-9398-30de3e8b64c2

--- src/move.h
+++ src/move.h
@@ -1,17 +1,26 @@
 #ifndef STONEAGE_MOVE_H
 #define STONEAGE_MOVE_H
 
-typedef double[7][2] *Trajectory;
-typedef int[7][2] *PixelTrajectory;
+#include "common.h"
 
-enum TrajectoryType
+typedef enum
 {
-    TR_Linear;
-};
+    TR_Linear = 0,
+    Trajectory_count
+} Trajectory;
+
+struct Move_impl;
 
 CLASS(Move)
 {
     INHERIT(Object);
-}
+
+    struct Move_impl *pimpl;
+    Move next;
+    Move prev;
+
+    void FUNC(init) ARG(int x, int y, int dx, int dy, Trajectory t);
+    int FUNC(step) ARG(int *x, int *y);
+};
 
 #endif
