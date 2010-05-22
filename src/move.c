16e06759d6776f195eab125e4d40761c6c7741e0
zirias <zirias@1df5a459-922f-45a1-9398-30de3e8b64c2>
2010-05-22 17:38:34 +0000

    compiles ...
    git-svn-id: https://svn.home.palmen-it.de/repo/stoneage/branches/z@82 1df5a459-922f-45a1-9398-30de3e8b64c2

--- src/move.c
+++ src/move.c
@@ -2,20 +2,17 @@
 
 #include <stdlib.h>
 
-typedef double[7][2] TrajectoryTable;
-typedef int[7][2] PixTrajTable;
-
 struct Move_impl
 {
     int x;
     int y;
-    int dy;
-    int dy;
+    int tx;
+    int ty;
     int step;
-    PixTrajTable *t;
+    int (*t)[7][2];
 };
 
-static const TrajectoryTable[Trajectory_count] tTables = {
+static const double tTables[Trajectory_count][7][2] = {
     {
 	{.125,.125},
 	{.25,.25},
@@ -27,7 +24,7 @@ static const TrajectoryTable[Trajectory_count] tTables = {
     }
 };
 
-static PixTrajTable[Trajectory_count];
+static int pTables[Trajectory_count][7][2];
 
 static int tile_width;
 static int tile_height;
@@ -39,18 +36,18 @@ computePixelTrajectories(void)
 
     for (i=0; i<Trajectory_count; ++i) for (j=0; j<7; ++j)
     {
-	PixTrajTable[i][j][0] = (int)tile_width*TrajectoryTable[i][j][0];
-	PixTrajTable[i][j][1] = (int)tile_height*TrajectoryTable[i][j][1];
+	pTables[i][j][0] = (int) tile_width * tTables[i][j][0];
+	pTables[i][j][1] = (int) tile_height * tTables[i][j][1];
     }
 }
 
 static void
-m_init ARG(int x, int y, int dy, int dy, Trajectory t)
+m_init ARG(int x, int y, int tx, int ty, Trajectory t)
 {
     METHOD(Move);
 
-    int w = abs(dx-x);
-    int h = abs(dy-y);
+    int w = abs(tx-x);
+    int h = abs(ty-y);
 
     if ((tile_height != h) || (tile_width != w))
     {
@@ -62,10 +59,10 @@ m_init ARG(int x, int y, int dy, int dy, Trajectory t)
     struct Move_impl *p = this->pimpl;
     p->x = x;
     p->y = y;
-    p->dx = dx;
-    p->dy = dy;
+    p->tx = tx;
+    p->ty = ty;
     p->step = 0;
-    p->t = &PixTrajTable[t];
+    p->t = &(pTables[t]);
 }
 
 static int
@@ -73,6 +70,24 @@ m_step ARG(int *x, int *y)
 {
     METHOD(Move);
 
+    int dx;
+    int dy;
+
+    struct Move_impl *p = this->pimpl;
+    if (p->step == 7)
+    {
+	*x = p->tx;
+	*y = p->ty;
+	return 1;
+    }
+    
+    dx = p->tx - p->x;
+    dy = p->ty - p->y;
+    *x = p->x + (int) dx * (*p->t)[p->step][0];
+    *y = p->y + (int) dy * (*p->t)[p->step][1];
+    p->step++;
+
+    return 0;
 }
 
 CTOR(Move)
