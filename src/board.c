144676863aedc346a075ea98110ce936ffe3ed84
zirias <zirias@1df5a459-922f-45a1-9398-30de3e8b64c2>
2010-08-28 05:16:24 +0000

    refactoring, moving screen related functionality of Board to Screen (WIP, broken)
    git-svn-id: https://svn.home.palmen-it.de/repo/stoneage/branches/z@117 1df5a459-922f-45a1-9398-30de3e8b64c2

--- src/board.c
+++ src/board.c
@@ -4,8 +4,6 @@
 
 #include "board.h"
 #include "screen.h"
-#include "resfile.h"
-#include "resource.h"
 #include "app.h"
 #include "event.h"
 #include "level.h"
@@ -57,12 +55,6 @@ struct Board_impl
 
     SDL_TimerID moveticker;
 
-    int off_x;
-    int off_y;
-
-    int tile_width;
-    int tile_height;
-
     int num_rocks;
     int num_cabbages;
     int level;
@@ -86,89 +78,6 @@ createMoveTickEvent(Uint32 interval, void *param)
     RaiseEvent(e);
     
     return interval;
-}
-
-SDL_Surface *
-loadPngSurface(Resource r)
-{
-    SDL_Surface *png;
-    SDL_RWops *rw = SDL_RWFromMem((void *)r->getData(r), r->getDataSize(r));
-    png = IMG_LoadPNG_RW(rw);
-    SDL_FreeRW(rw);
-    return png;
-}
-
-SDL_Surface *
-rotateSurface(SDL_Surface *s, int rot)
-{
-    int x,y;
-    uint32_t *srcpix, *dstpix;
-
-    SDL_Surface *dest = SDL_CreateRGBSurface(SDL_SWSURFACE,
-	    (rot % 2) ? s->w : s->h,
-	    (rot % 2) ? s->h : s->w, 
-	    32,
-	    s->format->Rmask,
-	    s->format->Gmask,
-	    s->format->Bmask,
-	    s->format->Amask);
-    SDL_LockSurface(s);
-    SDL_LockSurface(dest);
-    srcpix = s->pixels;
-    dstpix = dest->pixels;
-    if (rot == 1)
-    {
-	for (x=dest->w-1;x>=0;--x) for (y=0;y<dest->h;++y)
-	    dstpix[y*dest->w+x] = *srcpix++;
-    }
-    else if (rot == 2)
-    {
-	for (y=dest->h-1;y>=0;--y) for (x=dest->w-1;x>=0;--x)
-	    dstpix[y*dest->w+x] = *srcpix++;
-    }
-    else
-    {
-	for (x=0;x<dest->w;++x) for (y=dest->h-1;y>=0;--y)
-	    dstpix[y*dest->w+x] = *srcpix++;
-    }
-    SDL_UnlockSurface(s);
-    SDL_UnlockSurface(dest);
-    return dest;
-}
-
-SDL_Surface *
-scaleSurface(SDL_Surface *s, int width, int height, int rot)
-{
-    SDL_Surface *tile;
-    SDL_Surface *tmp;
-
-    if (rot)
-    {
-	tile = rotateSurface(s, rot);
-	tmp = zoomSurface (tile, (double)width/64,
-		(double)height/64, SMOOTHING_ON);
-	SDL_FreeSurface(tile);
-    }
-    else
-    {
-	tmp = zoomSurface (s, (double)width/64,
-		(double)height/64, SMOOTHING_ON);
-    }
-    tile = SDL_DisplayFormatAlpha(tmp);
-    SDL_FreeSurface(tmp);
-    return tile;
-}
-
-SDL_Surface *
-createScaledSurface(Resource r, int width, int height)
-{
-    SDL_Surface *png;
-    SDL_Surface *tile;
-
-    png = loadPngSurface(r);
-    tile = scaleSurface(png, width, height, 0);
-    SDL_FreeSurface(png);
-    return tile;
 }
 
 static void
@@ -710,21 +619,8 @@ CTOR(Board)
 {
     struct Board_impl *b;
     Resfile rf;
-#ifndef SDL_IMG_OLD
-    int imgflags;
-#endif
 
     BASECTOR(Board, EHandler);
-
-#ifndef SDL_IMG_OLD
-    imgflags = IMG_Init(IMG_INIT_PNG);
-    if (!(imgflags & IMG_INIT_PNG))
-    {
-	log_err("SDL_image library is missing PNG support!\n"
-		"Try installing libpng and zlib.\n");
-	mainApp->abort(mainApp);
-    }
-#endif
 
     b = XMALLOC(struct Board_impl, 1);
     memset(b, 0, sizeof(struct Board_impl));
@@ -792,10 +688,6 @@ DTOR(Board)
     DELETE(Resource, b->t_rock);
     DELETE(Resource, b->t_cabbage);
     DELETE(Resource, b->t_willy);
-
-#ifndef SDL_IMG_OLD
-    IMG_Quit();
-#endif
 
     XFREE(this->pimpl);
     BASEDTOR(EHandler);
